600c9b2b8da105049fccca13cd109bd33e961d2c
Jenya.Seletsky <js646@me.com>
2022-04-05 20:24:47 -0600

    courses

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,16 +1,18 @@
 import axios from "axios"
 
 const Courses = () => {
-
+const getCourses = () => {
+    axios.get("/courses").then()
+}
     return (
         <div>
              <h1>
                  Courses
-
              </h1>
             </div> 
     )
-
     }
 
     export default Courses
+
+    //
