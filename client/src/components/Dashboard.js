4d5a6b361d7433eaff876e3f72d6efd52e48f633
Stephen Moore <judo2000@comcast.net>
2022-04-07 19:21:40 -0600

    Display user information on dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,15 @@
+import jwt from "jwt-decode";
 import Auth from "../utils/auth";
 import { useQuery } from "@apollo/client";
 import { GET_ME } from "../utils/queries";
 
 const Dashboard = () => {
   const token = Auth.loggedIn() ? Auth.getToken() : null;
-  console.log(token)
+  const user = jwt(token);
+  console.log(user.data.firstName);
   const { data, loading } = useQuery(GET_ME);
   const userData = data?.me || {};
-console.log(token.firstName)
+  console.log(userData);
   if (!token) {
     return false;
   }
@@ -20,7 +22,7 @@ console.log(token.firstName)
       <br />
       <br />
       <h1>Dashboard</h1>
-      <h2>{userData.firstName}</h2>
+      <h2>Welcome to your profile, {user.data.firstName}</h2>
     </div>
   );
 };
