e9bf6bd432764aef31635ac9eeae659e7f00714b
Jenya.Seletsky <js646@me.com>
2022-04-12 21:24:24 -0600

    footer added

---
+++ client/src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+      <footer id="footer" className="footer text-center">
+    Code in Mind All Rights Reserved @ 2022 
+      </footer>
+    );
+  };
+  export default Footer;
