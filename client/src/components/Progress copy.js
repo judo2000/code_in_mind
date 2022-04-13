37650be716e8c7bed1e1978e2243ed862ea318ce
Stephen Moore <judo2000@comcast.net>
2022-04-13 07:49:13 -0600

    Add styling to footer

---
+++ client/src/components/Progress copy.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
+export default function CircularIndeterminate() {
+  // Fixed Spinner
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+}
