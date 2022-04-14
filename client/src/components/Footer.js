df58570d9dd2b0c04ae204a4340cbc55cc701afb
Jenya.Seletsky <js646@me.com>
2022-04-13 22:20:30 -0600

    updated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,24 @@ import "./footer.styles.css";
 const Footer = () => {
   return (
     <footer id="footer" className="footer text-center">
-      Code in Mind All Rights Reserved @ 2022
+      
+            <div className="row">
+                <div className="col l12" >
+                    <p className="white-text"></p>
+                    <a className="hoverable" href="https://linktr.ee/matthewbrignola" target="_blank">Matt
+                        Brignola</a> |
+                    <a className="hoverable" href="https://linktr.ee/AndrewMcInally" target="_blank">Andrew
+                        Mclnally</a> |
+                    <a className="hoverable" href="https://linktr.ee/stephensmoore" target="_blank">Stephen Moore</a> |
+                    <a className="hoverable" href="https://linktr.ee/jenya10016" target="_blank">Jenya
+              Seletsky</a>
+          <h5 className="white-text">Code in Mind  All Rights Reserved @ 2022</h5>
+          <div class="container">
+            
+                </div>
+            </div>
+        </div>
+
     </footer>
   );
 };
